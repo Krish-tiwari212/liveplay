b49cfdace221de506abb469310ff301e1f0e1b8b
404DEVR <rdev36437@gmail.com>
2024-10-28 20:56:42 +0530

    matrisc

--- frontend/components/EventMatrics.tsx
+++ frontend/components/EventMatrics.tsx
@@ -1,23 +1,72 @@
 import React from 'react'
 import { Button } from './ui/button';
 import { useRouter } from 'next/navigation';
+import { FaBarsProgress, FaUserGroup } from 'react-icons/fa6';
+import { IoBarChart } from 'react-icons/io5';
+import { FaChartPie, FaRupeeSign, FaStar } from 'react-icons/fa';
+import { SlCalender } from "react-icons/sl";
+import { GiSparklingSabre, GiTrophyCup } from "react-icons/gi";
+import { TiChartBarOutline } from "react-icons/ti";
+import { MdChecklist } from 'react-icons/md';
+import { GoSponsorTiers } from "react-icons/go";
 
 const keyMetrics = [
-  { title: 'Total Participants', value: 1000, change: '+10%', icon: 'icon-user-group' },
-  { title: 'Tickets Sold', value: 800, change: '-5%', icon: 'icon-bar-chart' },
-  { title: 'Revenue Generated', value: '50,000', change: '+15%', icon: 'icon-dollar-sign' },
-  { title: 'Active Teams', value: 5, change: '0%', icon: 'icon-calendar' },
-  { title: 'New Registrations', value: 150, change: '+20%', icon: 'icon-sparkline' },
-  { title: 'Event Ratings', value: '4.5/5', change: 'N/A', icon: 'icon-star' },
-  { title: 'Conversion Rate', value: '25%', change: '+5%', icon: 'icon-pie-chart' },
-  { title: 'Top Categories', value: 'Soccer, Basketball', change: 'N/A', icon: 'icon-bar-chart' },
+  {
+    title: "Total Participants",
+    value: 1000,
+    change: "+10%",
+    icon: <FaUserGroup />,
+  },
+  { title: "Tickets Sold", value: 800, change: "-5%", icon: <IoBarChart /> },
+  {
+    title: "Revenue Generated",
+    value: "50,000",
+    change: "+15%",
+    icon: <FaRupeeSign />,
+  },
+  { title: "Active Teams", value: 5, change: "0%", icon: <SlCalender /> },
+  {
+    title: "New Registrations",
+    value: 150,
+    change: "+20%",
+    icon: <GiSparklingSabre />,
+  },
+  { title: "Event Ratings", value: "4.5/5", change: "N/A", icon: <FaStar /> },
+  {
+    title: "Conversion Rate",
+    value: "25%",
+    change: "+5%",
+    icon: <FaChartPie />,
+  },
+  {
+    title: "Top Categories",
+    value: "Soccer, Basketball",
+    change: "N/A",
+    icon: <TiChartBarOutline />,
+  },
 ];
 
 const secondaryMetrics = [
-  { title: 'Top Performing Teams', data: ['Team A - 300', 'Team B - 250'], icon: 'icon-trophy' },
-  { title: 'Check-in Statistics', data: ['Checked In: 900', 'Registered: 1000'], icon: 'icon-checklist' },
-  { title: 'Sponsor Contributions', data: ['Sponsor A: 10,000', 'Sponsor B: 5,000'], icon: 'icon-sponsor' },
-  { title: 'Completion Rate', data: ['Completed: 80%'], icon: 'icon-progress' },
+  {
+    title: "Top Performing Teams",
+    data: ["Team A - 300", "Team B - 250"],
+    icon: <GiTrophyCup />,
+  },
+  {
+    title: "Check-in Statistics",
+    data: ["Checked In: 900", "Registered: 1000"],
+    icon: <MdChecklist />,
+  },
+  {
+    title: "Sponsor Contributions",
+    data: ["Sponsor A: 10,000", "Sponsor B: 5,000"],
+    icon: <GoSponsorTiers />,
+  },
+  {
+    title: "Completion Rate",
+    data: ["Completed: 80%"],
+    icon: <FaBarsProgress />,
+  },
 ];
 
 interface EventMatricsProps{
@@ -41,7 +90,6 @@ const EventMatrics = ({ handleNext }: EventMatricsProps) => {
 
       <main className="p-5 bg-white rounded-lg shadow-lg">
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {/* Key Metrics Cards */}
           {keyMetrics.map((metric, index) => (
             <div
               key={index}
@@ -49,10 +97,10 @@ const EventMatrics = ({ handleNext }: EventMatricsProps) => {
             >
               <h2 className="text-lg font-semibold">{metric.title}</h2>
               <p className="text-xl">
-                {metric.value}{" "}
+                {metric.value}
                 <span className="text-gray-500">({metric.change})</span>
               </p>
-              <i className={metric.icon + " text-gray-800"}></i>
+              {metric.icon}
             </div>
           ))}
         </section>
@@ -70,7 +118,7 @@ const EventMatrics = ({ handleNext }: EventMatricsProps) => {
                   <li key={idx}>{item}</li>
                 ))}
               </ul>
-              <i className={metric.icon + " text-gray-800"}></i>
+              {metric.icon}
             </div>
           ))}
         </section>
